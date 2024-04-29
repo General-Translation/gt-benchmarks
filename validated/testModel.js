9bd68c645096e57f1e4306c61c79fefeb7c8a25d
faviles28 <faviles@princeton.edu>
2024-04-29 10:11:25 -0400

    dataaaaa

--- validated/testModel.js
+++ validated/testModel.js
@@ -49,7 +49,7 @@ async function askQuestion(question) {
 async function processCSV(filePath, csvWriter) {
     let questions = [];
     let correctCount = 0;
-    const batchSize = 10;
+    const batchSize = 50;
     const delayTime = 1000;
 
     return new Promise((resolve, reject) => {
